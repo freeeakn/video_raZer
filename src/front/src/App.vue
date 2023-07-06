2d568afe48cf3f8feeeaf8cc541954579dd009dd
“freeeakn” <1rycont@gmail.com>
2023-07-07 04:41:53 +0700

    rebuild front

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -1,13 +1,68 @@
 <script setup>
 import Header from "./components/Header.vue"
 import Footer from "./components/Footer.vue"
+import { ref } from 'vue'
+import axios from 'axios'
+
+const submit = ref(false);
+const url = ref('');
+const answer = ref('');
+
+const formSubmit = () => {
+
+    let data = new FormData();
+    data.append('url', url.value);
+    data.append('csrfmiddlewaretoken', '{{csrf_token}}');
+    
+    submit.value = true;
+    try {
+        setTimeout(routerFunc, 3000);
+        axios.post('api/', data)
+            .then(response => {
+              answer.value = JSON.stringify(response, null, 2)
+            })
+            .catch(error => {
+              answer.value = 'Error: ' + error.response.status
+            })
+    } catch (err) {
+        console.error(err.toJSON());
+    }
+};
 </script>
 
 <template>
   <div class="bg-gray-900 text-white">
         <div class="animation flex flex-col justify-around h-screen w-full">
           <Header />
-          
+          <main class="flex justify-center items-center">
+              <form @submit.prevent.stop="formSubmit" class="m-5 p-5" v-if="!submit">
+                  <div class="flex flex-col">
+                      <label for="default-input" class="block mb-2 font-medium">Input url:</label>
+                      <input v-model="url" placeholder="VideoUrl..." type="url" name="url" class="bg-white text-gray-900 text-sm rounded-xl ring-4 focus:ring-lime-400  block p-2 w-72 drop-shadow-[0_35px_35px_rgb(163_230_53_/_0.09)]">
+                  </div>
+              </form>
+              <div v-if="submit">
+                  <AppLoad />
+              </div>
+          </main>
+
+          <!-- <main class="flex flex-col items-center justify-center p-5">
+              <div class="m-2 p-2 xl:w-1/2 drop-shadow-[0_20px_20px_rgb(163_230_53_/_0.15)]">
+                  <section  class="mb-16">
+                      <img class="w-full mb-5" src="https://randompicturegenerator.com/img/national-park-generator/gebf2d43a7afe4f731303406b059dd0aeed652937b8880225bbaf7c8b064374526b88012f8f26cc7883954cebc4f02996_640.jpg" alt="screenShot">
+                      <a href="#">{{ start }}</a>
+                      <p>
+                          {{ text }}
+                      </p>
+                  </section>
+              </div>
+              <router-link to="/">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+              </svg>
+              </router-link>
+          </main> -->
+
           <Footer />
         </div>
   </div>
@@ -17,5 +72,22 @@ import Footer from "./components/Footer.vue"
 @import url('https://fonts.googleapis.com/css2?family=Mulish:wght@300;500;800;900&display=swap');
 * {
     font-family: 'Mulish', sans-serif;
+}
+
+.animation {
+  transform: scale(0.75);
+  opacity: 0;
+  animation: ani 1s forwards;
+}
+
+@keyframes ani {
+  0% {
+    transform: scale(0.75);
+    opacity: 0;
+}
+  100% {
+    opacity: 1;
+    transform: scale(1);
+}
 }
 </style>
--- src/front/src/main.js
+++ src/front/src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')

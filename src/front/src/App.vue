46061709f6dea4d76aba36d6b8bdb45f6acf5538
“freeeakn” <1rycont@gmail.com>
2023-07-07 04:47:20 +0700

    add loading

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from "./components/Header.vue"
 import Footer from "./components/Footer.vue"
+import AppLoad from "./components/AppLoad.vue"
 import { ref } from 'vue'
 import axios from 'axios'
 
--- src/front/src/main.js
+++ src/front/src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from 'axios'
 
 const app = createApp(App)
 app.mount('#app')
